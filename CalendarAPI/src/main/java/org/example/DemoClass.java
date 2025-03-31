a6b7a907549595c4df77acad1175874f7c6c8ac1
jroswprovey <jroswprovey@gmail.com>
2025-03-31 15:54:12 -0400

    Implements spring boot

---
+++ CalendarAPI/src/main/java/org/example/DemoClass.java
@@ -0,0 +1,80 @@
+/*
+Joseph Rosw-Provey
+3/26/2025
+
+This is a demo class and will not be used in the end product and is just a POC
+
+*/
+
+package org.example;
+
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+
+import java.util.List;
+
+
+
+public class DemoClass {
+
+        public static void main(String[] args) {
+            try {
+
+                DateTime now = new DateTime(System.currentTimeMillis());
+                Calendar service = CalendarServiceFactory.getCalendarService();
+
+                CalendarList calendarList = service.calendarList().list().execute();
+                List<CalendarListEntry> calendarListItems = calendarList.getItems();
+
+                //Loops through the calendar list and Gets the name (Summary) and its ID.
+                for (CalendarListEntry listEntry : calendarListItems){
+                    System.out.println(
+                            "Name: " +listEntry.getSummary() + " " +
+                                    "Id: " + listEntry.getId()
+                    );
+                }
+
+                //Pulls events from Google using a calendar ID and puts that data into eventlist
+                Events events = service.events().list("lstd69l1aru7q4hc4g7drourh3fm86sd@import.calendar.google.com")
+                        .setMaxResults(10)
+                        .setTimeMin(now)
+                        .setOrderBy("startTime")
+                        .setSingleEvents(true)
+                        .execute();
+                List<Event> eventsList = events.getItems();
+
+                //goes through every event and prints out Name (or summary as google calls it) then gets date and time
+                for (Event event : eventsList) {
+                    System.out.println(
+                            "Name: " + event.getSummary() + " " +
+                                    "Start Date & Time: " + event.getStart().getDateTime().toStringRfc3339() + " " +
+                                    "Time Zone: " + event.getStart().getTimeZone()
+                    );
+                }
+
+                //creating a new Test event
+                Event testEvent = new Event();
+
+
+                //Creates a new EVENT date time object to be used in the event creation
+                EventDateTime start = new EventDateTime().setDateTime(now);
+
+
+                //setting parameters for event
+                testEvent.setSummary("Test Event")
+                        .setStart(start)
+                        .setEnd(start);
+
+                //sending event off to google
+                service.events().insert("primary", testEvent).execute();
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
--- CalendarAPI/src/main/java/org/example/Main.java
+++ CalendarAPI/src/main/java/org/example/Main.java
@@ -6,70 +6,16 @@
 
 package org.example;
 
-import com.google.api.client.util.DateTime;
-import com.google.api.services.calendar.model.*;
-import org.example.CalendarServiceFactory;
-import com.google.api.services.calendar.Calendar;
 
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
 
-            DateTime now = new DateTime(System.currentTimeMillis());
-            Calendar service = CalendarServiceFactory.getCalendarService();
-
-            CalendarList calendarList = service.calendarList().list().execute();
-            List<CalendarListEntry> calendarListItems = calendarList.getItems();
-
-            //Loops through the calendar list and Gets the name (Summary) and its ID.
-            for (CalendarListEntry listEntry : calendarListItems){
-                System.out.println(
-                       "Name: " +listEntry.getSummary() + " " +
-                                "Id: " + listEntry.getId()
-                );
-            }
-
-            //Pulls events from Google using a calendar ID and puts that data into eventlist
-            Events events = service.events().list("lstd69l1aru7q4hc4g7drourh3fm86sd@import.calendar.google.com")
-                    .setMaxResults(10)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
-            List<Event> eventsList = events.getItems();
-
-            //goes through every event and prints out Name (or summary as google calls it) then gets date and time
-            for (Event event : eventsList) {
-                System.out.println(
-                        "Name: " + event.getSummary() + " " +
-                        "Start Date & Time: " + event.getStart().getDateTime().toStringRfc3339() + " " +
-                        "Time Zone: " + event.getStart().getTimeZone()
-                );
-            }
-
-            //creating a new Test event
-            Event testEvent = new Event();
-
-
-            //Creates a new EVENT date time object to be used in the event creation
-            EventDateTime start = new EventDateTime().setDateTime(now);
-
-
-            //setting parameters for event
-            testEvent.setSummary("Test Event")
-                    .setStart(start)
-                    .setEnd(start);
-
-            //sending event off to google
-            service.events().insert("primary", testEvent).execute();
-
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
