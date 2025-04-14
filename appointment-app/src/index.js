f18a89531e40589818d35936d4b1097be152f65c
alejandromoncadag <alex.squashplayer@gmail.com>
2025-04-14 19:55:04 -0400

    Receive token from Google Sign-in on the front end and send it back to back end

--- appointment-app/src/index.js
+++ appointment-app/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Alex 
+//  receive the Google token in your React frontend and send it to your Spring Boot backend.
+import { GoogleOAuthProvider } from "@react-oauth/google";
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // insert google client id - alex 
+  <GoogleOAuthProvider clientId="1085714409158-abcxyz123.apps.googleusercontent.com">
     <App />
-  </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
